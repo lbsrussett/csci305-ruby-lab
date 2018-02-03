7fa7f20b61768709e1d736371d3025510d7a5cd6
lbsrussett <lbsrussett@gmail.com>
2018-02-03 12:12:31 -0700

    cleaning up repo

--- ruby_lab.rb
+++ ruby_lab.rb
@@ -4,13 +4,13 @@
 #
 # CSCI 305 - Ruby Programming Lab
 #
-# <firstname> <lastname>
-# <email-address>
+# Laura Sullivan-Russett
+# lbsrussett@gmail.com
 #
 ###############################################################
 
 $bigrams = Hash.new # The Bigram data structure
-$name = "<firstname> <lastname>"
+$name = "Laura Sullivan-Russett"
 
 # function to process each line of a file and extract the song titles
 def process_file(file_name)
@@ -18,14 +18,25 @@ def process_file(file_name)
 
 	begin
 		IO.foreach(file_name) do |line|
+			title = clean_title(line)
+			puts title 
+			
 			# do something for each line
 		end
-
 		puts "Finished. Bigram model built.\n"
 	rescue
 		STDERR.puts "Could not open file"
 		exit 4
 	end
+end
+
+def clean_title(line)
+	str = line.dup
+	str.sub!(/%(([A-Z0-9]+)<([A-Z]+)>)+/, "")
+	str.sub!(/\A.*<([A-Z]+)>/, "")
+	str.sub!(/[\(\[\{\\\/_\-:\"`\+=\*].*/, "")
+	str.sub!(/\bfeat\b.*/, "")
+	return str
 end
 
 # Executes the program
